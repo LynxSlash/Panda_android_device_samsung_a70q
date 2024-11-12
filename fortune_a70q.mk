3ab01fc63283232e3b38424d5b268ead25ffa79d
lynx <122455759+LynxSlash@users.noreply.github.com>
2024-11-12 23:23:41 +0500

    Update and rename lineage_a70q.mk to fortune_a70q.mk

--- fortune_a70q.mk
+++ fortune_a70q.mk
@@ -11,11 +11,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from a70q device
 $(call inherit-product, device/samsung/a70q/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common fortune stuff.
+$(call inherit-product, vendor/fortune/config/common_full_phone.mk)
+
+FORTUNE_KEEPER := Geeky
+TARGET_SUPPORTS_BLUR := true
+TARGET_USES_BLUR_RECENT := false
+TARGET_USES_LAWNCHAIR := true
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_a70q
+PRODUCT_NAME := fortune_a70q
 PRODUCT_DEVICE := a70q
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A705
