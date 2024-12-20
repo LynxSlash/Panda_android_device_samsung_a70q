e2c82b3ee16d1818322c2e46307e20f8ee8bdbd5
lynx <122455759+LynxSlash@users.noreply.github.com>
2024-12-20 08:29:13 -0800

    Adapt to yaap

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a70q.mk
+    $(LOCAL_DIR)/yaap_a70q.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_a70q-user \
-    lineage_a70q-userdebug \
-    lineage_a70q-eng
+    yaap_a70q-user \
+    yaap_a70q-userdebug \
+    yaap_a70q-eng
